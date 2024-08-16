98ff19fbd5707dbc57c3675700c98043d6f4da9c
daniel commins <daniel.commins@techelevator.com>
2024-08-15 17:57:30 -0700

    initial draft

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+  initializeGame();
+});
+
+function initializeGame() {
+
+  // Get the game table
+  const gameTableEl = document.getElementById("game-table");
+
+  // Add table rows, i.e. tr elements
+
+    // Add table cells, i.e. td elements
+
+
+  // Add table row to table
+
+}
+
+function toggleLights(elementClicked) {
+
+  // toggle the light that was clicked
+
+  // toggle the TOP light
+
+  // toggle the BOTTOM light
+
+  // toggle the LEFT light
+
+  // toggle the RIGHT light
+
+}
+
+function toggleLight(element) {
+  // Get the current background color
+
+  // element.style.backgroundColor changes the color
+
+}
